09247bea8a417f0b3e8109d5c20552dabf1894f5
Poshitha Ravindu <raviposhitha@gmail.com>
2024-08-10 11:13:24 +0530

    fix: replacing port

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,14 @@ import { Logger } from '@nestjs/common';
 import * as dotenv from 'dotenv';
 
 async function bootstrap() {
-  const port = 8080;
+ 
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   app.enableCors();
   dotenv.config();
+  
+  const port = process.env.PORT || 8080;
   await app.listen(port);
   Logger.log(
     `🚀 Submit jokes Application is running on: http://localhost:${port}/${globalPrefix}`,
